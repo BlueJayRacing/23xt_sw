50f10fca46bea0bcb5cc93a567f2b941d7305c7e
Stefanos <tedlastefanos@gmail.com>
2026-09-23 01:49:17 -0400

    finished the plots

--- software/rpm_analysis/05-10-26-OregonDay4/splittables.m
+++ software/rpm_analysis/05-10-26-OregonDay4/splittables.m
@@ -1,10 +1,13 @@
+% change the path to where your data is
 cd C:\Users\steve\baja\23xt_sw\software\rpm_analysis\05-10-26-OregonDay4
 folderpath = pwd;
 fileExtension = '*.csv';
 filelist = dir(fullfile(folderpath, fileExtension));
+% filelist sorts by alphabetical order
 
 %% 
-
+% skipped the first file because its bugged for some reason
+% the last file in filelist is a combo of all the data from each csv file
 for m = 2:length(filelist) 
 
     file = filelist(m).name;
@@ -17,6 +20,7 @@ for m = 2:length(filelist)
         temp_table(:, 1) = [];
         temp_table(:, 2) = [];
         myTables{m-1,n} = temp_table;
+        % column 1 is channel 22, column 2 is channel 23, etc.
     end
         array22 = table2array(myTables{m-1, 1});
         array23 = table2array(myTables{m-1, 2});
@@ -25,6 +29,7 @@ for m = 2:length(filelist)
         array26 = table2array(myTables{m-1, 5});
         array27 = table2array(myTables{m-1, 6});
 
+        f = figure('Visible', 'off');
         t = tiledlayout(3,2);
         nexttile;
         plot(array22(:, 1), array22(:, 2));
@@ -38,9 +43,11 @@ for m = 2:length(filelist)
         plot(array26(:, 1), array26(:, 2));
         nexttile;
         plot(array27(:, 1), array27(:, 2));
+        % the plot is value of the channel against relative time
 
         plotval = m-1;
         layoutname = "layout" + plotval + ".pdf";
         exportgraphics(t, layoutname);
 end
 
+t
---
+++ software/rpm_analysis/05-10-26-OregonDay4/wsg data seperated/ReadMe.txt
@@ -0,0 +1,7 @@
+column 1 is the data from channel 22, column 2 is the data from channel 23, etc.
+
+each row is a different csv file
+
+the first row of the cell array is the second alphabetical file of the data because the first one is bugged
+
+the last row of the cell array is all the data of the csv files combined, indexed by relative time (so it does have a lot of overlap)
